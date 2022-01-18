c43fb3c8d0cf0d06f8c0928435ef8d0e66557bcc
dus22 <dus22@LAPTOP-QAEFIRH8>
2022-01-19 00:07:18 +0900

    2022-01-18

---
+++ Programmers/src/oneCommit/Ex03.java
@@ -0,0 +1,30 @@
+package oneCommit;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		Solution3 sol = new Solution3();
+		int[] absolutes = {4,7,12};
+		boolean[] signs = {true,false,true};
+		
+		int answer = sol.solution(absolutes, signs);
+		System.out.println(answer);
+	}
+}
+
+class Solution3 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        int i=0;
+        for(i=0;i<signs.length;i++){
+            if(signs[i]){
+                //true
+                answer += absolutes[i];
+            }else {
+                //false
+                answer -= absolutes[i];
+            }
+        }
+        
+        return answer;
+    }
+}
