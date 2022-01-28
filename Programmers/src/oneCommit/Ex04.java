c20b2306c1267062ac61188ddf3e7db92b65643d
suyeonChoi7807 <suyeon2205@naver.com>
2022-01-28 17:27:18 +0900

    22-01-28

---
+++ Programmers/src/oneCommit/Ex04.java
@@ -0,0 +1,28 @@
+package oneCommit;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		Solution4 sol =  new Solution4();
+		System.out.println(sol.solution(5));
+	}
+}
+
+class Solution4 {
+	  public String solution(int n) {
+	      String[] numbers = {"4", "1", "2"};
+	      String answer = "";
+	      
+	      int num = n;
+	      
+	      while(num > 0){
+	          int remainder = num % 3;
+	          num /= 3;
+	          
+	          if(remainder == 0) num--;
+	          
+	          answer = numbers[remainder] + answer;
+	      }
+	      
+	      return answer;
+	  }
+	}
